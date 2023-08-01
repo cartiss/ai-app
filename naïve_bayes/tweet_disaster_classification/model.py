b5f4942849776eb4ba9aa86809ed532b75802144
Kyryl Fesiuk <71554932+cartiss@users.noreply.github.com>
2023-08-01 20:30:27 +0200

    Tweet sentiment analysis (#10)
    * Add Tweet disaster classification model, add text parser
    
    * Update model and text formatter, add methods for server implementation
    
    * Delete unused libs
    
    * Generate new poetry lock
    
    * Create sentiment analysis model
    
    * Add dataset downloading
    
    ---------
    
    Co-authored-by: algindanil <algindanil.st@gmail.com>

---
+++ naïve_bayes/tweet_disaster_classification/model.py
@@ -0,0 +1,96 @@
+"""Naive Bayes model which predicts whether the tweet is about disastrous events based on its text."""
+import json
+
+import pandas as pd
+
+
+class NaiveBayesModel:
+    """Class for Naive Bayes model determining whether a tweet is about a disaster based on its text."""
+
+    def __init__(self) -> None:
+        """Initializes class fields."""
+        self.text_samples = None
+        self.labels = None
+        self.freq_dict = None
+        self.class_freq = None
+
+    def train(self, text_samples: pd.Series, labels: pd.Series) -> None:
+        """
+        Updates the model's parameters with values from the training data.
+
+        :param text_samples: pd.Series containing processed text samples.
+        :param labels: pd.Series containing labels for the text samples with the corresponding index.
+        """
+        # calculate class frequencies
+        self.class_freq[0] = len(self.labels.loc[self.labels == 0])
+        self.class_freq[1] = len(self.labels.loc[self.labels == 1])
+
+        # calculate occurrences of each word in the training data
+        for _, (text, label) in pd.concat([text_samples, labels], axis=1).iterrows():
+            for word in text:
+                if word not in self.freq_dict:
+                    self.freq_dict[word] = {0: 0, 1: 0}
+
+                match label:
+                    case 0:
+                        self.freq_dict[word][0] += 1
+                    case 1:
+                        self.freq_dict[word][1] += 1
+
+    def predict(self, text_samples: pd.Series) -> pd.Series:
+        """
+        Predicts class labels for each example in the given processed text samples and returns
+        a pd.Series with predictions for text samples with the corresponding index.
+        :param text_samples: pd.Series with tweet texts for prediction.
+        """
+        predictions = []
+
+        for _, text in text_samples:
+            prob_dict = {0: 1.0, 1: 1.0}
+            for word in text:
+                if word in self.freq_dict:
+                    for i in [0, 1]:
+                        prob_dict[i] *= self.freq_dict[word][i] / self.class_freq[i]
+
+            for i in [0, 1]:
+                prob_dict[i] *= self.class_freq[i]
+
+            predictions.append(max(prob_dict, key=prob_dict.get))
+
+        return pd.Series(predictions, index=text_samples.index)
+
+    def export_parameters_to_json(self, class_freq_path: str, words_freq_path: str) -> None:
+        """
+        Exports current model parameters to .json files.
+        :param class_freq_path: the path to the .json file to store the dictionary with class frequencies.
+        :param words_freq_path: the path to the .json file to store the dictionary with frequencies for each word.
+        """
+        json_freq_dict = json.dumps(self.freq_dict)
+        json_class_freq = json.dumps(self.class_freq)
+
+        try:
+            with open(class_freq_path, mode='w', encoding='utf-8') as file:
+                file.write(json_class_freq)
+        except FileNotFoundError:
+            print('File not found. Export terminated.')
+            return
+        except OSError:
+            print('Error opening the file. Export terminated.')
+
+        # TODO: exception handling
+        with open(words_freq_path, mode='w', encoding='utf-8') as file:
+            file.write(json_freq_dict)
+
+    def import_parameters(self, class_freq_path: str, words_freq_path: str) -> None:
+        """
+        Imports model parameters from .json files.
+        :param class_freq_path: the path to the .json file with the dictionary with class frequencies.
+        :param words_freq_path: the path to the .json file with the dictionary with frequencies for each word.
+        """
+
+        # TODO: exception handling
+        with json.open(class_freq_path, mode='r', encoding='utf-8') as file:
+            self.class_freq = json.loads(file.read())
+
+        with json.open(words_freq_path, mode='r', encoding='utf-8') as file:
+            self.freq_dict = json.loads(file.read())
