742ba458bc8a41e9637caa9b0cb279ee12bc0e3f
NikitaVishnyak <82162976+NikitaVishnyak@users.noreply.github.com>
2023-09-14 23:54:13 +0200

    Make new design

--- app/utils.py
+++ app/utils.py
@@ -4,6 +4,7 @@ from typing import List
 import pandas as pd
 # from naïve_bayes.sentiment_analysis.model import SentimentAnalysisModel
 # from naïve_bayes.tweet_disaster_classification.model import NaiveBayesModel
+from models import db, Project, Category
 
 
 def make_sentiment_prediction(text) -> pd.Series:
@@ -54,3 +55,64 @@ def compile_results(prediction_sentiment, prediction_disaster) -> List[str]:
     logging.error(results)
 
     return results
+
+def make_homepage_queryset() -> dict:
+    """
+        Make queryset of categories and their projects.
+
+        :return: dict with categories and their projects
+    """
+    categories_and_projects = db.session.query(Category, Project).join(Project,
+                                                                       Category.id == Project.category_id).all()
+    category_project_dict = {}
+
+    for category, project in categories_and_projects:
+        if category not in category_project_dict:
+            category_project_dict[category] = []
+        category_project_dict[category].append(project)
+
+    return category_project_dict
+
+def make_category_projects_queryset(category_name):
+    """
+        Make queryset of category and projects of this category.
+
+        :param category_name: str with name of category
+        :return: Category object and list of projects
+    """
+    prepared_category_name = prepare_category_name(category_name)
+    category = Category.query.filter_by(name=str(prepared_category_name)).first()
+    if category:
+        projects = Project.query.filter_by(category_id=category.id).all()
+        return category, projects
+    else:
+        return None, []
+
+
+
+
+def prepare_category_name(category_name) -> str:
+    """
+        Format the name of category.
+
+        :param category_name: str with name of category
+        :return: formatted str
+    """
+    if len(category_name) == 3:
+        return category_name.upper()
+    else:
+        return category_name.capitalize()
+
+
+# def make_vision_projects_queryset(category_name):
+#     category = Category.query.filter_by(name='Vision').first()
+#     projects = Project.query.filter_by(category_id=category.id).all()
+#
+#     return category, projects
+#
+# def make_tabular_projects_queryset(category_name):
+#     res = prepare_category_name(category_name)
+#     category = Category.query.filter_by(name='Vision').first()
+#     projects = Project.query.filter_by(category_id=category.id).all()
+#
+#     return category, projects
--- app/web.py
+++ app/web.py
@@ -5,43 +5,61 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import desc
 
-
-from utils import make_sentiment_prediction, make_disaster_prediction, compile_results
+from utils import make_sentiment_prediction, make_disaster_prediction, compile_results, make_homepage_queryset, make_category_projects_queryset
+from models import db, app, Project, Tag, ProjectTag, Category
 
 postgres_name = os.getenv('SQL_DB')
-app = Flask(__name__)
+# app = Flask(__name__)
 app.config['SECRET_KEY'] = 'iejowda32msflsn3dkf7jnad9mk1lpd'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ai_db_user:Hgyfshbjs8482@localhost/aiprojectsdb'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ai_db_user:Hgyfshbjs8482@localhost/aiprojectsdb'
 app.config['UPLOAD_FOLDER'] = 'static'
-db = SQLAlchemy(app)
 
 
-
-class Project(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
-    description = db.Column(db.Text)
-    image_url = db.Column(db.String(255))
-    tags = db.relationship('Tag', secondary='project_tag', backref='projects')
-    url = db.Column(db.String(255), unique=True)
+# db = SQLAlchemy(app)
 
 
-class Tag(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
-
-
-class ProjectTag(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
-    tag_id = db.Column(db.Integer, db.ForeignKey('tag.id'))
+# class Project(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(255))
+#     description = db.Column(db.Text)
+#     category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+#     category = db.relationship('Category', backref='projects')
+#     url = db.Column(db.String(255), unique=True)
+#     image_url = db.Column(db.String(255))
+#     tags = db.relationship('Tag', secondary='project_tag', backref='projects')
+#
+#
+# class Category(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(255))
+#
+#
+# class Tag(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(255))
+#
+#
+# class ProjectTag(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
+#     tag_id = db.Column(db.Integer, db.ForeignKey('tag.id'))
 
 
 @app.route("/")
-def index() -> None:
+def index() -> str:
     """Render home page."""
-    projects = Project.query.order_by(Project.id).all()
-    return render_template('index.html', projects=projects)
+    category_project_dict = make_homepage_queryset()
+
+    return render_template('index.html', category_project_dict=category_project_dict)
+
+
+@app.route("/<category_name>")
+def category_projects(category_name) -> str:
+    """Render page with specific category of project."""
+    category, projects = make_category_projects_queryset(category_name)
+
+    return render_template('filtered_projects.html', category=category, projects=projects)
+
 
 
 @app.route("/tweet-model/", methods=["GET", "POST"])
@@ -57,8 +75,6 @@ def tweet_model() -> str:
         return render_template('tweet_model.html', title="Tweet Model", results=results)
 
     return render_template('tweet_model.html', title="Tweet Model")
-
-
 
 
 def main() -> None:
