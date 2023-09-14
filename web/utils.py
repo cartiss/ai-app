e1c9ceea25590573a1f953b46813075201e835ed
NikitaVishnyak <82162976+NikitaVishnyak@users.noreply.github.com>
2023-09-15 00:26:06 +0200

    Make code refactoring

--- web/utils.py
+++ web/utils.py
@@ -2,8 +2,8 @@ import logging
 from typing import List
 
 import pandas as pd
-# from naïve_bayes.sentiment_analysis.model import SentimentAnalysisModel
-# from naïve_bayes.tweet_disaster_classification.model import NaiveBayesModel
+from naïve_bayes.sentiment_analysis.model import SentimentAnalysisModel
+from naïve_bayes.tweet_disaster_classification.model import NaiveBayesModel
 from models import db, Project, Category
 
 
--- web/web.py
+++ web/web.py
@@ -1,48 +1,12 @@
 """Run Flask application."""
-import os
+from flask import render_template, request
 
-from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import desc
+from utils import make_sentiment_prediction, make_disaster_prediction, compile_results, make_homepage_queryset, \
+    make_category_projects_queryset
+from models import db, app
 
-from utils import make_sentiment_prediction, make_disaster_prediction, compile_results, make_homepage_queryset, make_category_projects_queryset
-from models import db, app, Project, Tag, ProjectTag, Category
-
-postgres_name = os.getenv('SQL_DB')
-# app = Flask(__name__)
 app.config['SECRET_KEY'] = 'iejowda32msflsn3dkf7jnad9mk1lpd'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ai_db_user:Hgyfshbjs8482@localhost/aiprojectsdb'
 app.config['UPLOAD_FOLDER'] = 'static'
-
-
-# db = SQLAlchemy(app)
-
-
-# class Project(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(255))
-#     description = db.Column(db.Text)
-#     category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
-#     category = db.relationship('Category', backref='projects')
-#     url = db.Column(db.String(255), unique=True)
-#     image_url = db.Column(db.String(255))
-#     tags = db.relationship('Tag', secondary='project_tag', backref='projects')
-#
-#
-# class Category(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(255))
-#
-#
-# class Tag(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(255))
-#
-#
-# class ProjectTag(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
-#     tag_id = db.Column(db.Integer, db.ForeignKey('tag.id'))
 
 
 @app.route("/")
@@ -59,7 +23,6 @@ def category_projects(category_name) -> str:
     category, projects = make_category_projects_queryset(category_name)
 
     return render_template('filtered_projects.html', category=category, projects=projects)
-
 
 
 @app.route("/tweet-model/", methods=["GET", "POST"])
